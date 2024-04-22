509911765dab373f136da2cb5edf84f1b348686f
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-04-22 20:32:43 +0700

    adding divider

---
+++ src/components/divider.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Divider, AbsoluteCenter } from "@chakra-ui/react";
+
+export default function CustomDivider() {
+    return (
+        <Box position='relative' padding='10'>
+            <Divider borderColor="black" />
+            <AbsoluteCenter bg='white' px='4'>
+                Projects
+            </AbsoluteCenter>
+        </Box>
+    )
+}
--- src/components/homeScreen/coverComponent.tsx
+++ src/components/homeScreen/coverComponent.tsx
@@ -1,1 +1,40 @@
+"use client"
 import React from "react";
+import { Stack, Center, Box, Flex, Button, Heading } from "@chakra-ui/react";
+import { DownloadIcon } from '@chakra-ui/icons'
+
+export default function CoverComponent(){
+    return (
+        <Box w='100%' paddingBlockStart={100}>
+            <Center>
+                <Heading >
+                    I'm Naufal Fikri Hakim.
+                </Heading>
+            </Center>
+            <Center pt={5}>
+                <Flex>
+                    <a href="https://github.com/NaufalFikriHakim">
+                        <img src="/github-mark.svg" width="40px" height="40px" alt="" />
+                    </a>
+                    <a href="https://gitlab.com/naufal.fikri11">
+                        <img src="https://images.ctfassets.net/xz1dnu24egyd/4V92fFTJOIlTPHHzSdfxem/3fdc9f0d82f08ed4c355c6e4126b870c/gitlab-logo-600.svg" width="40px" height="40px" alt="" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/naufal-fikri-hakim/">
+                        <img src="/icons8-linkedin-50.svg" width="40px" height="40px" alt="" />
+                    </a>
+                </Flex>
+            </Center>
+            <Center pt={5}>
+                <Flex gap="2rem">
+                    <Button colorScheme='gray' rightIcon={<DownloadIcon />}>
+                        Resume
+                    </Button>
+                    <Button colorScheme='gray'>
+                        Contact
+                    </Button>
+                </Flex>
+
+            </Center>
+        </Box>
+    )
+}
